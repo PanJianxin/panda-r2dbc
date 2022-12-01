d380f2206aa6a0166c06916a4126152ba4b69bbe
Panda <239692275@qq.com>
2022-12-01 17:52:50 +0800

    精简配置文件

---
+++ panda-r2dbc-spring-boot/panda-r2dbc-spring-boot-autoconfigure/src/main/java/com/jxpanda/autoconfigure/R2dbcAutoConfiguration.java
@@ -0,0 +1,114 @@
+package com.jxpanda.autoconfigure;
+
+import com.jxpanda.r2dbc.spring.data.convert.MappingReactiveConverter;
+import com.jxpanda.r2dbc.spring.data.convert.R2dbcCustomTypeHandlers;
+import com.jxpanda.r2dbc.spring.data.core.ReactiveEntityTemplate;
+import com.jxpanda.r2dbc.spring.data.extension.constant.StringConstant;
+import com.jxpanda.r2dbc.spring.data.extension.support.IdGenerator;
+import com.jxpanda.r2dbc.spring.data.extension.support.SnowflakeIdGenerator;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.convert.CustomConversions;
+import org.springframework.data.r2dbc.convert.R2dbcCustomConversions;
+import org.springframework.data.r2dbc.dialect.DialectResolver;
+import org.springframework.data.r2dbc.dialect.R2dbcDialect;
+import org.springframework.data.r2dbc.mapping.R2dbcMappingContext;
+import org.springframework.data.relational.core.mapping.DefaultNamingStrategy;
+import org.springframework.data.relational.core.mapping.NamingStrategy;
+import org.springframework.r2dbc.core.DatabaseClient;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+
+@Configuration
+@EnableConfigurationProperties(R2dbcProperties.class)
+public class R2dbcAutoConfiguration {
+
+    private final DatabaseClient databaseClient;
+
+    private final R2dbcDialect dialect;
+
+    private final R2dbcProperties mappingProperties;
+
+
+    public R2dbcAutoConfiguration(DatabaseClient databaseClient, R2dbcProperties mappingProperties) {
+        this.databaseClient = databaseClient;
+        this.dialect = DialectResolver.getDialect(this.databaseClient.getConnectionFactory());
+        this.mappingProperties = mappingProperties;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public ReactiveEntityTemplate r2dbcEntityTemplate(MappingReactiveConverter r2dbcConverter) {
+        return new ReactiveEntityTemplate(this.databaseClient, this.dialect, r2dbcConverter);
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public MappingReactiveConverter r2dbcConverter(R2dbcMappingContext mappingContext,
+                                                   R2dbcCustomConversions r2dbcCustomConversions,
+                                                   R2dbcCustomTypeHandlers r2dbcCustomTypeHandlers) {
+        return new MappingReactiveConverter(mappingContext, r2dbcCustomConversions, r2dbcCustomTypeHandlers, this.mappingProperties.transfer());
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public R2dbcMappingContext r2dbcMappingContext(ObjectProvider<NamingStrategy> namingStrategy,
+                                                   R2dbcCustomConversions r2dbcCustomConversions) {
+        R2dbcMappingContext relationalMappingContext = new R2dbcMappingContext(
+                namingStrategy.getIfAvailable(() -> DefaultNamingStrategy.INSTANCE));
+        relationalMappingContext.setForceQuote(this.mappingProperties.isForceQuote());
+        relationalMappingContext.setSimpleTypeHolder(r2dbcCustomConversions.getSimpleTypeHolder());
+        return relationalMappingContext;
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public NamingStrategy namingStrategy() {
+        return this.mappingProperties.getNamingPolicy();
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public R2dbcCustomConversions r2dbcCustomConversions() {
+        List<Object> converters = new ArrayList<>(this.dialect.getConverters());
+        converters.addAll(R2dbcCustomConversions.STORE_CONVERTERS);
+        return new R2dbcCustomConversions(
+                CustomConversions.StoreConversions.of(this.dialect.getSimpleTypeHolder(), converters),
+                Collections.emptyList());
+    }
+
+    @Bean
+    @ConditionalOnMissingBean
+    public R2dbcCustomTypeHandlers r2dbcCustomTypeHandlers() {
+        return new R2dbcCustomTypeHandlers();
+    }
+
+
+    @Bean
+    @ConditionalOnMissingBean
+    public IdGenerator<?> idGenerator() {
+
+        return new SnowflakeIdGenerator<String>(this.mappingProperties.getDataCenterId(), this.mappingProperties.getWorkerId()) {
+            @Override
+            protected String cast(Long id) {
+                return id.toString();
+            }
+
+            @Override
+            public boolean isIdEffective(String id) {
+                return !StringConstant.ID_DEFAULT_VALUES.contains(id);
+            }
+        };
+    }
+
+
+}
